0c8684fc9f21eee5fcd9b5a6626ec5724f847e25
Danielle Twaalfhoven <danielle@wetransfer.com>
2017-07-31 22:07:52 +0200

    playing around with signin out

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute } from 'react-router'
+import store, { history } from './store'
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import injectTapEventPlugin from 'react-tap-event-plugin'
+
+import App from './App';
+import SignIn from './users/SignIn'
+import SignUp from './users/SignUp'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <Route path="/sign-up" component={SignUp} />
+        <Route path="/sign-in" component={SignIn} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
+
 registerServiceWorker();
