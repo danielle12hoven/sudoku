b29b236d9a676a9e025dc8ce3fa2c4372f3015ac
Danielle Twaalfhoven <danielle@wetransfer.com>
2017-08-01 08:34:57 +0200

    added nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ import { push } from 'react-router-redux'
 import signOut from '../actions/user/sign-out'
 import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton'
-import LocalDining from 'material-ui/svg-icons/maps/local-dining'
+import ActionHome from 'material-ui/svg-icons/action/home';
 import FlatButton from 'material-ui/FlatButton'
 
 class Navigation extends PureComponent {
@@ -29,7 +29,7 @@ class Navigation extends PureComponent {
     return (
       <AppBar
         title="Sudoku"
-        iconElementLeft={<IconButton onClick={this.goHome}><LocalDining /></IconButton>}
+        iconElementLeft={<IconButton onClick={this.goHome}><ActionHome /></IconButton>}
         iconElementRight={signedIn ?
           <FlatButton label="Sign out" onClick={signOut} /> :
           <FlatButton label="Sign up" onClick={this.signUp} />
